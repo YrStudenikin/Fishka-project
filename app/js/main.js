c3c1b93b50e6e200ce8c493036cd6cab3d311383
tranzet23 <tranzet23@github.com>
2021-05-25 15:15:58 +0300

    top bar

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function () {
-
+    $(document).ready(function(){
+        $('.btn-bento__inner').click(function(){
+            $('.btn-bento__inner').toggleClass('active');
+        })
+    })
 })
